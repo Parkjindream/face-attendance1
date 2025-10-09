0d4bd6a48ee2f02716499f6bef302b461352caa9
Supich555 <ssse4764@gmail.com>
2025-10-09 21:17:01 +0700

    Update face_utils.py
    Signed-off-by: Supich555 <ssse4764@gmail.com>

--- face_utils.py
+++ face_utils.py
@@ -4,3 +4,9 @@ import numpy as
 import os
 from models import get_connection
 from config import ENCODINGS_DIR, FACE_MATCH_THRESHOLD
+
+# --- Load known encodings from database ---
+def load_known_encodings():
+    conn = get_connection()
+  cursor = conn.cursor()
+    cursor.execute("SELECT student_id, encoding FROM encodings")
