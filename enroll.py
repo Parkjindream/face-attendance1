f7a960c4ac16e6cd2e55f83c3eceab08ac5f71e6
WanitchaJP <wanitchajabprangg@gmail.com>
2025-10-11 23:45:56 +0700

    Define enroll_student function structure
    Signed-off-by: WanitchaJP <wanitchajabprangg@gmail.com>

--- enroll.py
+++ enroll.py
@@ -4,3 +4,6 @@ import os
 from models import add_student, get_connection
 from face_utils import get_face_encoding
 from config import ENCODINGS_DIR
+
+def enroll_student(student_id, name, image_path):
+  pass
